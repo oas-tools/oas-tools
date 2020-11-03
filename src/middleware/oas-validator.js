63e580708c273f87bedc4bc8c062214c0ade6df1
Patryk Laurent <patryk.laurent@dmgt.com>
2020-11-03 14:05:07 +0000

    revert accidental commit (restore async)

--- src/middleware/oas-validator.js
+++ src/middleware/oas-validator.js
@@ -100,7 +100,7 @@ function addFilesToJSONPropertyValidation(files, dataToValidate) {
  * @param {string} method - Method requested by the client.
  * @param {string} req - The whole req object from the client request.
  */
-function checkRequestData(oasDoc, requestedSpecPath, method, res, req, next) { // eslint-disable-line
+async function checkRequestData(oasDoc, requestedSpecPath, method, res, req, next) { // eslint-disable-line
   var paths = oasDoc.paths;
   var keepGoing = true;
   //var msg = "";
@@ -136,12 +136,7 @@ function checkRequestData(oasDoc, requestedSpecPath, method, res, req, next) { /
           for (let url of missingReferences) {
             logger.info("OASValidator Getting remote reference '" + url +"'")
             try {
-              fetch(url).then(response =>
-                response.text().then(body => {
-
-                })
-              )
-
+              const response = await fetch(url)
               const body = await response.text()
               if (body[0] == "o") { // openapi yaml
                 await validator.setRemoteReference(url, yaml.safeLoad(body));
