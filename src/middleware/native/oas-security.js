f6db8738bdf3b9e50ff0a1cf87ee99c3cba0195b
alesancor1 <alesancor1@alum.us.es>
2022-05-04 21:29:02 +0200

    feat: Implemented apikey auth and multiple schemes

--- src/middleware/native/oas-security.js
+++ src/middleware/native/oas-security.js
@@ -17,7 +17,7 @@ export class OASSecurity extends OASBase {
     if (!handlers && secSchemes) {
       throw new ConfigError("No security handlers defined in config for security schemes.");
     }
-    if (Object.keys(handlers).some(key => !secSchemes[key])) {
+    if (Object.keys(secSchemes).some(key => !handlers[key])) {
       throw new ConfigError("Missing handlers for some of the security schemes declared in OAS Document.");
     }
     if (handlers && Object.values(handlers).some(handler => typeof handler !== 'function')) {
@@ -25,27 +25,42 @@ export class OASSecurity extends OASBase {
     }
 
     /* Instanciate middleware */
-    return new OASSecurity(oasFile, (req, res, next) => {
+    return new OASSecurity(oasFile, async (req, res, next) => {
       const oasRequest = oasFile.paths[req.route.path][req.method.toLowerCase()];
       if (oasFile.security || oasRequest.security) {
         const secReqs = oasRequest.security ?? oasFile.security;
-        secReqs.flatMap(i => Object.entries(i)).forEach(([secName, secScopes]) => {
-          const secDef = secSchemes[secName];
-          let token;
-          if (!secDef) {
-            throw new ConfigError(`Security scheme '${secName}' not found in OAS Document.`);
+        /* Logical OR */
+        await Promise.any(secReqs.map(async secReq => {
+          /* Logical AND */
+          Object.entries(secReq).forEach(([secName, secScope]) => {
+            const secDef = secSchemes[secName];
+            let token;
+            if (!secDef) {
+              throw new ConfigError(`Security scheme '${secName}' not found in OAS Document.`);
+            }
+            switch (secDef.type) {
+              case 'http':
+                token = req.headers.authorization; break;
+              case 'apiKey':
+                if (secDef.in === 'header') {
+                  token = req.headers[secDef.name.toLowerCase()];
+                } else if (secDef.in === 'query') {
+                  token = req.query[secDef.name];
+                } else {
+                  token = req.headers.cookie?.split(';').find(c => c.trim().startsWith(`${secDef.name}=`))?.split('=')[1];
+                } break;
+              case 'oauth2':
+                throw new UnsupportedError(`oauth2 security schemes are not supported yet.`);
+              default:
+                throw new UnsupportedError(`Security scheme ${secName} is invalid or not supported.`);
+            }
+            if (!token) throw new SecurityError(`Missing token for security scheme ${secName}.`);
+            handlers[secName](token, (status) => res.status(status));
+          });
+        })).catch(err => {
+          if (err instanceof AggregateError) {
+            next(err.errors[0]);
           }
-          switch (secDef.type) {
-            case 'http':
-              token = req.headers.authorization; break;
-            case 'apiKey':
-            case 'oauth2':
-              throw new UnsupportedError(`apiKey and oauth2 security schemes are not supported yet.`);
-            default:
-              throw new UnsupportedError(`Security scheme ${secName} is invalid or not supported.`);
-          }
-          if (!token) throw new SecurityError(`Missing token for security scheme ${secName}.`);
-          handlers[secName](token, (status) => res.status(status));
         });
       } else {
         logger.debug('No security defined for this request, skipping security middleware.');
