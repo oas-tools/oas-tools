dc57b062c6120cbc97512f72ad74b625cdd3c6e3
alesancor1 <alesancor1@alum.us.es>
2022-07-29 20:58:59 +0200

    feat: Complete backwards compatibility

---
+++ src/entrypoint.cjs
@@ -0,0 +1,24 @@
+const oasOld = import('./index_old.js');
+const oasNew = import('./index.js');
+
+function initialize() {
+    if (arguments.length === 3) {
+        return oasOld.then(m => m.initialize(arguments[0], arguments[1], arguments[2]));
+    } else {
+        return oasNew.then(m => m.initialize(arguments[0], arguments[1]));
+    }
+}
+
+function use() {
+    oasNew.then(m => m.use(arguments[0], arguments[1], arguments[2]))
+}
+
+function configure() {
+    oasOld.then(m => m.configure(arguments[0]));
+}
+
+module.exports = {
+    initialize,
+    configure,
+    use
+}
