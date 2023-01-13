466d85305384cf81e535bbf1556b4b74f2f45baf
Mohammad Umar <umarsoft.java@gmail.com>
2023-01-13 23:35:39 +0400

    Added fix for Cannot convert undefined or null to object for oas path params
    Added fix for Cannot convert undefined or null to object for oas path params
    
    2023-01-13 23:01:89 [oas-tools] ERROR: TypeError: Cannot convert undefined or null to object
        at Function.keys (<anonymous>)
        at _getParameterValue (file:///Users/acoder/Documents/GitHub/login-service/node_modules/@oas-tools/core/src/middleware/native/oas-params.js:93:36)
        at file:///Users/acoder/Documents/GitHub/login-service/node_modules/@oas-tools/core/src/middleware/native/oas-params.js:27:78

--- src/middleware/native/oas-params.js
+++ src/middleware/native/oas-params.js
@@ -20,7 +20,11 @@ export class OASParams extends OASBase {
                 const contentType = Object.keys(oasRequest.requestBody.content)[0];
                 body = schema.parseBody(body, oasRequest.requestBody.content[contentType].schema);
             }
-            Object.values(params).forEach((param) => paramsObj[param.name] = _getParameterValue(req, param));
+            Object.values(params).forEach((param) => {
+                if (param.name){
+                        paramsObj[param.name] = _getParameterValue(req, param);
+                             }
+             });
             res.defaultSend = res.send; // save original send for error handling
             res.locals.oas = { params: paramsObj, body: body };
             if(req.file || req.files && req.files.length > 0) res.locals.oas.files = [req.files, req.file].flat().filter((file) => file !== undefined);
