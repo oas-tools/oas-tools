638ce65e1fb9d79438b3e4af08e4b3f625beb763
Macedo, John <john.macedo@one.verizon.com>
2023-06-13 13:36:11 -0500

    fix: removed extraneous console.log

--- tests/suites/security.test.js
+++ tests/suites/security.test.js
@@ -170,7 +170,6 @@ export default () => {
                   })
                 })
                 .then( res => {
-                console.log(res.status);
                     assert("certCredentials" in global &&
                            "subject" in global.certCredentials &&
                            "CN" in global.certCredentials.subject,
